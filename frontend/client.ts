3ce2347ed10f0b2cc22dd19b5f7e4896ce09ed40
Leap <git@leap.new>
2026-01-02 00:15:08 +0000

    Document page styling properties

--- frontend/client.ts
+++ frontend/client.ts
@@ -134,6 +134,7 @@ export namespace auth {
  * Import the endpoint handlers to derive the types for the client.
  */
 import { create as api_games_create_create } from "~backend/games/create";
+import { deleteGame as api_games_delete_deleteGame } from "~backend/games/delete";
 import { exportGames as api_games_export_exportGames } from "~backend/games/export";
 import { get as api_games_get_get } from "~backend/games/get";
 import { list as api_games_list_list } from "~backend/games/list";
@@ -147,6 +148,7 @@ export namespace games {
         constructor(baseClient: BaseClient) {
             this.baseClient = baseClient
             this.create = this.create.bind(this)
+            this.deleteGame = this.deleteGame.bind(this)
             this.exportGames = this.exportGames.bind(this)
             this.get = this.get.bind(this)
             this.list = this.list.bind(this)
@@ -160,6 +162,12 @@ export namespace games {
             // Now make the actual call to the API
             const resp = await this.baseClient.callTypedAPI(`/games`, {method: "POST", body: JSON.stringify(params)})
             return JSON.parse(await resp.text(), dateReviver) as ResponseType<typeof api_games_create_create>
+        }
+
+        public async deleteGame(params: { id: number }): Promise<ResponseType<typeof api_games_delete_deleteGame>> {
+            // Now make the actual call to the API
+            const resp = await this.baseClient.callTypedAPI(`/games/${encodeURIComponent(params.id)}`, {method: "DELETE", body: undefined})
+            return JSON.parse(await resp.text(), dateReviver) as ResponseType<typeof api_games_delete_deleteGame>
         }
 
         public async exportGames(): Promise<ResponseType<typeof api_games_export_exportGames>> {
